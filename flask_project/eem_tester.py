9655e8d4bfb3f4849fe21569a4c222328c23359b
Dwayne <dwayne.camacho1277@gmail.com>
2023-12-18 14:41:35 -0500

    latest working eem test and all supporting files to include python venv

---
+++ flask_project/eem_tester.py
@@ -0,0 +1,59 @@
+from jinja2 import Template
+import yaml
+
+# Your YAML data
+yaml_data = """
+EEMs:
+  - name: MAIN
+    commands: 
+    - event manager applet {{ eem.name }} authorization bypass
+    - event none maxrun 60
+    action:    
+    - "enable"
+    - "configure terminal"
+    - "interface f0/1"
+    - "description MAIN"
+    - "switchport mode access vlan 100"
+    - "end"
+
+  - name: BRANCH
+    commands: 
+    - event manager applet {{ eem.name }} authorization bypass
+    - event none maxrun 60
+    action:    
+    - "enable"
+    - "configure terminal"
+    - "interface f0/2"
+    - "description BRANCH"
+    - "switchport mode access vlan 200"
+    - "end"
+"""
+
+# Your Jinja template
+jinja_template = """
+{# Iterate through EEMs #}
+{% for eem in EEMs %}
+{% if eem.name in reqdEEMs %}
+{% for command in eem.commands %}
+{{ command }}
+{% endfor %}
+{% for action in eem.action %}
+action {{ loop.index }}.0 cli command "{{ action }}"
+{% endfor %}
+{% endif %}
+{% endfor %}
+
+
+"""
+
+# Create a Jinja template object
+template = Template(jinja_template)
+
+# Load YAML data into a dictionary
+yaml_dict = yaml.safe_load(yaml_data)
+
+# Render the template with the YAML data
+output_text = template.render(reqdEEMs=["BRANCH"], EEMs=yaml_dict.get("EEMs", {}))
+
+# Print the output
+print(output_text)
