00b0d43ddfec8131509837e5760e11ffa9f74928
Dwayne <dwayne.camacho1277@gmail.com>
2024-01-03 22:49:51 -0500

    removed comment in factorial.py

--- wgu_python_course/factorial.py
+++ wgu_python_course/factorial.py
@@ -7,7 +7,7 @@ total = N
 i = N - 1 # Initialize the loop variable
 
 while i >= 1:
-    total = total * i # Set total to total * (i)
+    total = total * i 
     i = i - 1 # Decrement i
 
 print(total)
