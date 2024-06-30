2332318ddd0478df19c37ca3dc433cc8d75729e6
Dwayne <dwayne.camacho1277@gmail.com>
2024-06-30 17:28:55 -0400

    removed debug on bat

--- EOP ETP/start_drink_app.bat
+++ EOP ETP/start_drink_app.bat
@@ -1,38 +1,20 @@
 @echo off
 :: Navigate to the directory containing your Flask app
 cd /d "C:\Users\dwayn\Documents\Git\devnet-devfun\EOP ETP"
-if %errorlevel% neq 0 (
-    echo Failed to navigate to Flask app directory
-    pause
-    exit /b 1
-)
 
 :: Activate the virtual environment
 call venv\Scripts\activate
-if %errorlevel% neq 0 (
-    echo Failed to activate virtual environment
-    pause
-    exit /b 1
-)
 
 :: Set the FLASK_APP environment variable
 set FLASK_APP=drink_tracker_app.py
 
 :: Run the Flask application
-python -m flask run
-if %errorlevel% neq 0 (
-    echo Failed to start Flask application
-    pause
-    exit /b 1
-)
+start "" python -m flask run
+
+:: Wait for a few seconds to allow the Flask server to start
+timeout /t 5 /nobreak
 
 :: Open the default web browser and navigate to the Flask app
 start "" "http://127.0.0.1:5000/"
-if %errorlevel% neq 0 (
-    echo Failed to open web browser
-    pause
-    exit /b 1
-)
 
-echo Flask application started successfully
-pause
+:: End of the script
