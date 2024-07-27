0621ce20266673e9cebf26f748b3f6d75287906d
Dwayne Camacho <dwayne.camacho1277@gmail.com>
2024-07-27 20:12:42 +0300

    streamlined bat file

--- EOP ETP/streamlit_drink_app.bat
+++ EOP ETP/streamlit_drink_app.bat
@@ -1,14 +1,12 @@
 @echo off
-REM Change to the directory where the Streamlit app is located
+:: Change to the directory where the Streamlit app is located
 cd /d "C:\Users\CIS_User\Documents\git\devnet-devfun\EOP ETP"
 
-REM Activate the virtual environment if you are using one
-REM If not using a virtual environment, you can remove or comment out the following line
-REM call C:\Users\CIS_User\Documents\git\devnet-devfun\EOP ETP\Scripts\activate
+:: Activate the virtual environment
 call venv\Scripts\activate
 
-REM Launch the Streamlit app
+:: Launch the Streamlit app
 start "" streamlit run drinktracker_app.py
 
-REM Keep the command prompt open after the script finishes
+:: Keep the command prompt open after the script finishes
 pause
